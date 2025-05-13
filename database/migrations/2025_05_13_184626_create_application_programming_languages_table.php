62ba038e883c82add03d853f28201cb7bf848be8
LadyPS15 <ladydiperezsimon@gmail.com>
2025-05-13 13:47:26 -0500

    Migraciones

---
+++ database/migrations/2025_05_13_184626_create_application_programming_languages_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('application_programming_languages', function (Blueprint $table) {
+            $table->foreignId('application_id')->constrained()->onDelete('cascade');
+            $table->foreignId('programming_language_id')->constrained();
+            $table->integer('proficiency_level')->default(3); // Level 1-5
+            $table->primary(['application_id', 'programming_language_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('application_programming_languages');
+    }
+};
